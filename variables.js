eef13d60f429f2669b67271f4959691820fe47bf
emivnajera <emivelasquez2000@hotmail.com>
2022-03-23 18:20:13 -0600

    se agrego el archivo de variables.

---
+++ variables.js
@@ -0,0 +1,21 @@
+
+var nombre = "Emiliano";
+
+//Declarar variable
+var edad;
+
+//Inicializar
+edad = 22;
+
+edad;
+
+
+var elementos = ["Compuatdora", "Celular"];
+
+elementos;
+
+var persona = {
+    nombre: "Emiliano",
+    edad: 30
+}
+
