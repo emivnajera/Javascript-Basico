ebc59d9c2c2fee180074125968553bf0f81a0eae
emivnajera <emivelasquez2000@hotmail.com>
2022-03-27 17:49:50 -0600

    se agrega el archivo de condicionales.

---
+++ Condicionales.js
@@ -0,0 +1,18 @@
+//IF
+var condicion;
+if (condicion){
+    console.log("Condicion Verdadera");
+}else{
+    console.log("Concicion False")
+}
+
+
+var edad;
+
+if(edad < 18){
+    console.log("No Puede Votar, Menor De Edad");
+}else if(edad > 60 ){
+    console.log("No puede Votar, Muy Mayor");
+}else{
+    console.log("Puede Votar");
+}
