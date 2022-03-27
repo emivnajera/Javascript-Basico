a2947f530d3e28aaca1e98421cb8e08d603fbccf
emivnajera <emivelasquez2000@hotmail.com>
2022-03-27 17:41:18 -0600

    se agrega el archivo de operadores.

---
+++ Operadores.js
@@ -0,0 +1,31 @@
+//Artimeticos
+3 + 2;
+50 - 10;
+10 * 20;
+20 / 2;
+//Concatenar
+"Emiliano" + "Velasquez";
+
+//Comparitivos (TODOS LEOS EJEMPLOS RETORNAN TRUE)
+3 == "3";
+3 === 3;
+3  > 2;
+3 < 5;
+3 >= 2;
+3 >= 3;
+3 <= 5;
+3 <= 3;
+
+//Logicos (TODOS LEOS EJEMPLOS RETORNAN TRUE)
+//Not
+!false;
+//And
+true && true;
+//Or
+true || false;
+ 
+
+//Incrementos y Decremetos
+var a = 4;
+a++; //Retorna 5
+a--; // Retorna 4
